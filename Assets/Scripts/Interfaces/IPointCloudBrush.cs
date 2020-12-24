c4d16a15f21ad82e88eaf63d8d198db78fdeb96e
Arniel <aceba1@outlook.com>
2020-12-24 01:18:25 -0500

    Begin point cloud brush implementable

---
+++ Assets/Scripts/Interfaces/IPointCloudBrush.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+interface IPointCloudBrush
+{
+    //void Modify(byte[,] cloud, int size, Vector2 offset, float pointSize, float rotation);
+
+    void Modify(byte[,] cloud, int squareCount, Vector2 offset, float pointSize);
+}
